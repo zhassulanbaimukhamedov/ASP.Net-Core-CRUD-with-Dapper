62e12225fdcca9f22a9fb13cbc3b43488cc76293
zhassulanbaimukhamedov <zasulanb@gmail.com>
2021-08-30 23:51:31 +0600

    Добавьте файлы проекта.

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        IUserRepository repo;
+        public HomeController(IUserRepository r)
+        {
+            repo = r;
+        }
+        public IActionResult Index()
+        {
+            return View(repo.GetUsers());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //public ActionResult Index()
+        //{
+        //    return View(repo.GetUsers());
+        //}
+
+        public ActionResult Details(int id)
+        {
+            User user = repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            repo.Create(user);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            User user = repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            repo.Update(user);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            User user = repo.Get(id);
+            if (user != null)
+                return View(user);
+            return NotFound();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            repo.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication5/Models/UserRepository.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+
+namespace WebApplication5.Models
+{
+    public interface IUserRepository
+    {
+        void Create(User user);
+        void Delete(int id);
+        User Get(int id);
+        List<User> GetUsers();
+        void Update(User user);
+    }
+    public class UserRepository : IUserRepository
+    {
+        string connectionString = null;
+        public UserRepository(string conn)
+        {
+            connectionString = conn;
+        }
+        public List<User> GetUsers()
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<User>("SELECT * FROM Users").ToList();
+            }
+        }
+
+        public User Get(int id)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<User>("SELECT * FROM Users WHERE Id = @id", new { id }).FirstOrDefault();
+            }
+        }
+
+        public void Create(User user)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "INSERT INTO Users (Name, Age) VALUES(@Name, @Age)";
+                db.Execute(sqlQuery, user);
+
+                // если мы хотим получить id добавленного пользователя
+                //var sqlQuery = "INSERT INTO Users (Name, Age) VALUES(@Name, @Age); SELECT CAST(SCOPE_IDENTITY() as int)";
+                //int? userId = db.Query<int>(sqlQuery, user).FirstOrDefault();
+                //user.Id = userId.Value;
+            }
+        }
+
+        public void Update(User user)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "UPDATE Users SET Name = @Name, Age = @Age WHERE Id = @Id";
+                db.Execute(sqlQuery, user);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "DELETE FROM Users WHERE Id = @id";
+                db.Execute(sqlQuery, new { id });
+            }
+        }
+    } 
+}
